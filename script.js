ff1a89b1411afbc49cbdf5058431a83e4e9125ef
Anfas-afk <kanfas888@gmail.com>
2026-01-30 15:55:51 +0530

    changed mobile reponsive header

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Initialize Lucide icons
 lucide.createIcons();
 
+// Initialize Lenis Smooth Scroll
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    direction: 'vertical',
+    gestureDirection: 'vertical',
+    smooth: true,
+    mouseMultiplier: 1,
+    smoothTouch: false,
+    touchMultiplier: 2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 // Mobile Menu Toggle (Robust & Smooth)
 const menuToggle = document.getElementById('menuToggle');
 const navContainer = document.querySelector('.nav-container');
